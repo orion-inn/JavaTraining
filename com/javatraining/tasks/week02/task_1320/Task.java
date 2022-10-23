23511398ffe211a88752799d5aeb70ae7779be6b
apetukhov <apetukhov@avaya.com>
2022-10-23 15:20:43 +0200

    task021320

---
+++ com/javatraining/tasks/week02/task_1320/Task.java
@@ -0,0 +1,25 @@
+package com.javatraining.tasks.week02.task_1320;
+
+public class Task {
+  public static void main(String[] args) throws Exception {
+    System.out.println(Matrix.NEO);
+  }
+
+  static class Matrix {
+    public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
+  }
+
+  interface DBObject {
+    DBObject initializeIdAndName(long id, String name);
+  }
+
+  static class User {
+    long id;
+    String name;
+
+    @Override
+    public String toString() {
+      return String.format("The user's name is %s, id = %d", name, id);
+    }
+  }
+}
