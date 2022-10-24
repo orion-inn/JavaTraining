849b91cf53c8ffff97a1af3b6827de2d02fa98a4
Ognjen Jakupovic <ognjen.jakupovic@orioninc.com>
2022-10-24 19:46:52 +0200

    Solved task_1320

--- src/com/javatraining/tasks/week02/task_1320/Task.java
+++ src/com/javatraining/tasks/week02/task_1320/Task.java
@@ -1,25 +1,30 @@
 package com.javatraining.tasks.week02.task_1320;
 
 public class Task {
-//  public static void main(String[] args) throws Exception {
-//    System.out.println(Matrix.NEO);
-//  }
-//
-//  static class Matrix {
-//    public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
-//  }
-//
-//  interface DBObject {
-//    DBObject initializeIdAndName(long id, String name);
-//  }
-//
-//  static class User {
-//    long id;
-//    String name;
-//
-//    @Override
-//    public String toString() {
-//      return String.format("The user's name is %s, id = %d", name, id);
-//    }
-//  }
+  public static void main(String[] args) throws Exception {
+    System.out.println(Matrix.NEO);
+  }
+
+  static class Matrix {
+    public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
+  }
+
+  interface DBObject {
+    DBObject initializeIdAndName(long id, String name);
+  }
+
+  static class User implements DBObject {
+    long id;
+    String name;
+
+    public User initializeIdAndName(long id, String name) {
+        this.id = id;
+        this.name = name;
+        return this;
+    }
+    @Override
+    public String toString() {
+      return String.format("The user's name is %s, id = %d", name, id);
+    }
+  }
 }
