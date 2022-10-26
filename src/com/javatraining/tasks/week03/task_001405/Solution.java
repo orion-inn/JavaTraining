2f05b937dda9e02d0c2c853cdcea722ba16cb9e6
Ognjen Jakupovic <ognjen.jakupovic@orioninc.com>
2022-10-26 22:45:38 +0200

    Solved task_001405

--- src/com/javatraining/tasks/week03/task_001405/Solution.java
+++ src/com/javatraining/tasks/week03/task_001405/Solution.java
@@ -1,7 +1,6 @@
 package src.com.javatraining.tasks.week03.task_001405;
 
 public class Solution {
-    /*
     public static void main(String[] args) {
         Food food = new Food();
         Selectable selectable = new Food();
@@ -12,20 +11,26 @@ public class Solution {
 
     public static void callFoodMethods(Food food) {
         //call methods
+        food.onSelect();
+        food.onEat();
     }
 
     public static void callSelectableMethods(Selectable selectable) {
         //call methods
+        selectable.onSelect();
     }
 
     interface Selectable {
         void onSelect();
     }
 
-    static class Food {
+    static class Food implements Selectable {
         public void onEat() {
             System.out.println("The food was eaten");
         }
+
+        public void onSelect() {
+            System.out.println("The food was selected.");
+        }
     }
-    */
 }
