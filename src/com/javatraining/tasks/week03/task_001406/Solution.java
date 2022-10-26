a9b1b4b8ea17f43e52981f778d80d74e9c3f9cd5
apetukhov <apetukhov@avaya.com>
2022-10-26 18:20:54 +0200

    tasks 03001405 03001406 03001409 03001410

---
+++ src/com/javatraining/tasks/week03/task_001406/Solution.java
@@ -0,0 +1,30 @@
+package src.com.javatraining.tasks.week03.task_001406;
+
+public class Solution {
+    public static void main(String[] args) {
+        Object obj = //Add your code here
+
+                Mouse mouse = (Mouse) obj;
+        GrayMouse grayMouse = (GrayMouse) mouse;
+        Jerry jerry = (Jerry) grayMouse;
+
+        printClasses(obj, mouse, grayMouse, jerry);
+
+    }
+
+    public static void printClasses(Object obj, Mouse mouse, GrayMouse grayMouse, Jerry jerry) {
+        System.out.println(jerry.getClass().getSimpleName());
+        System.out.println(grayMouse.getClass().getSimpleName());
+        System.out.println(mouse.getClass().getSimpleName());
+        System.out.println(obj.getClass().getSimpleName());
+    }
+
+    static class Mouse {
+    }
+
+    static class GrayMouse extends Mouse {
+    }
+
+    static class Jerry extends GrayMouse {
+    }
+}
