12af596b7901be407e699482a09e8689bd54ff28
Ognjen Jakupovic <ognjen.jakupovic@orioninc.com>
2022-11-03 16:22:26 +0100

    Solved task_001413

---
+++ src/com/javatraining/tasks/week04/task_001413/CompItem.java
@@ -0,0 +1,5 @@
+package src.com.javatraining.tasks.week04.task_001413;
+
+public interface CompItem {
+    String getName ();
+}
---
+++ src/com/javatraining/tasks/week04/task_001413/Monitor.java
@@ -0,0 +1,7 @@
+package src.com.javatraining.tasks.week04.task_001413;
+
+public class Monitor implements CompItem {
+    public String getName () {
+        return this.getClass().getSimpleName();
+    }
+}
