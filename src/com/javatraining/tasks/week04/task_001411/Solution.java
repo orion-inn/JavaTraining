494f77d5d7188a56152e1a92cd413a85afa1c049
Ognjen Jakupovic <ognjen.jakupovic@orioninc.com>
2022-11-03 16:03:20 +0100

    Solved task_001411

--- src/com/javatraining/tasks/week04/task_001411/Solution.java
+++ src/com/javatraining/tasks/week04/task_001411/Solution.java
@@ -10,8 +10,16 @@ public class Solution {
         String key = null;
 
         //key reading cycle here (1)
+        while ((key = reader.readLine()) != null && Key.has(key))
         {
             // create an object (2)
+            person = switch (key.toLowerCase()) {
+                case "user" -> new Person.User();
+                case "loser" -> new Person.Loser();
+                case "coder" -> new Person.Coder();
+                case "proger" -> new Person.Proger();
+                default -> throw new IllegalArgumentException("Allowed keys are: user, loser, coder, proger"); // never going to happen
+            };
 
             doWork(person); // call doWork
 
@@ -20,5 +28,31 @@ public class Solution {
 
     public static void doWork(Person person) {
         // (3)
+        if (person instanceof Person.User) {
+            ((Person.User) person).live();
+        } else if (person instanceof Person.Loser) {
+            ((Person.Loser) person).doNothing();
+        } else if (person instanceof Person.Coder) {
+            ((Person.Coder) person).writeCode();
+        } else if (person instanceof Person.Proger) {
+            ((Person.Proger) person).enjoy();
+        }
+    }
+}
+
+enum Key {
+    USER,
+    LOSER,
+    CODER,
+    PROGER;
+
+    public static boolean has (String key) {
+        for (Key value : Key.values()) {
+            if (value.name().equals(key.toUpperCase())) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
