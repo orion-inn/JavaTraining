015d67602fa8ccf495a1062bfd18f774da42610b
Ognjen Jakupovic <ognjen.jakupovic@orioninc.com>
2022-11-11 22:38:45 +0100

    Solved task_001421

--- src/com/javatraining/tasks/week05/task_001421/Solution.java
+++ src/com/javatraining/tasks/week05/task_001421/Solution.java
@@ -6,5 +6,9 @@ Singleton
 
 public class Solution {
     public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 + " == " + s2 + "? " + s1.equals(s2));
     }
 }
