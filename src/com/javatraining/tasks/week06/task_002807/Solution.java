ee249f66ab0966a7e5968a3fe66451d1c4dceea8
Ognjen Jakupovic <ognjen.jakupovic@orioninc.com>
2022-11-21 18:47:33 +0100

    Solved task_002807

--- src/com/javatraining/tasks/week06/task_002807/Solution.java
+++ src/com/javatraining/tasks/week06/task_002807/Solution.java
@@ -1,8 +1,29 @@
 package src.com.javatraining.tasks.week06.task_002807;
 
+import java.util.concurrent.*;
+
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
         // Add your code here
+        LinkedBlockingQueue<Runnable> runnableQueue = new LinkedBlockingQueue<>();
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i + 1;
+            runnableQueue.add(() -> doExpensiveOperation(finalI));
+        }
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 5, 1000, TimeUnit.MILLISECONDS, runnableQueue);
+        executor.prestartAllCoreThreads();
+
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
 
         /* Example output
 pool-1-thread-2, localId=2
